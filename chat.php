cce4e59d3b4623303b94898f38eaacbc5dc09fc5
Anaê Soares <anae08@gmail.com>
2019-10-14 16:52:23 -0300

    criado cha.php com menu lateral para ongs, e iniciado mensagens, incluido imagens como exemplo.

---
+++ chat.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/styles.css" media="screen" />
+    <link href="img/escudo.png" sizes="32x32" rel="shortcut icon"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Guardiões Pet</title>
+</head>
+<body>
+<div class="row">
+    <header class="col-lg-4 col-md-4 col-sm-6 col-xs-6 p-5 m-5">
+        <div class="card" style="width: 20rem;">
+            <img id="perfil-ong" src="img/foto_perfil_ong.jpg" alt="logo da ONG"/>
+            <div class="card-body" style="width: 20rem;">
+                <nav class="nav flex-column bg-light">
+                    <a class="nav-link menu-ong active" href="#">Meu Perfil</a>
+                    <a class="nav-link menu-ong" href="#">Animais Cadastrados</a>
+                    <a class="nav-link menu-ong" href="#">Cadastrar Novos Animais</a>
+                    <a class="nav-link menu-ong" href="#">Anúncios Cadastrados</a>
+                    <a class="nav-link menu-ong" href="#">Cadastrar Novos Anúncios</a>
+                    <a class="nav-link menu-ong" href="#">Mensagens</a>
+                    <a class="nav-link menu-ong-sair" href="#">Sair</a>
+                </nav>               
+            </div>    
+        </div>
+    </header>    
+    <section class="col-lg-4 col-md-4 col-sm-6 col-xs-6 d-flex justify-content-center" id="mensagens">
+        <div class="card-body">
+            <div class="mensagem-guardiao">
+            <img id="perfil-chat-guardiao" src="img/user.jpg" alt="foto perfil guardião"/>
+                <h6>Jone Doe</h6>
+                    <p>Assunto :Lar Temporário</p>
+            </div>
+            <div class="mensagem-guardiao">
+                <h6>Jane Doe</h6>
+                    <p>Assunto :Lar Temporário</p>
+            </div>
+            <div class="mensagem-guardiao">
+                <h6>Fulano Doe</h6>
+                    <p>Assunto :Lar Temporário</p>
+            </div>
+        </div>                    
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+
+        </div>
+    </section>
+
+</div>                
+</body>
+</html>
