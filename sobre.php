8fe0b7fc8cfab534053d2fb7dddb0967a175a137
Anaê Soares <anae08@gmail.com>
2019-10-16 15:43:38 -0300

    concluído página sobre, incluído css e imagem para a página

--- sobre.php
+++ sobre.php
@@ -12,13 +12,60 @@
 <body>
     <!--include do menu-->
     <main>
-        <section class="container d-flex flex-column justify-content-start align-items-center">
-            <h2>Quem Somos?</h2>
+        <div class="jumbotron">
+            <h1 class="display-4">Quem Somos</h1>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet nihil rerum obcaecati autem doloremque voluptatum iusto enim consequatur odio esse provident maxime, non natus. Possimus explicabo omnis distinctio? Cum.Fugit cumque officia, recusandae placeat aliquam pariatur magni natus sunt delectus eaque voluptatibus odit numquam corporis quis molestias. Tempore numquam quia illo quidem culpa accusamus molestiae recusandae explicabo eveniet nihil.</p>
+            <hr class="my-4 bg-white">
+            <h1 class="display-4">O que fazemos?</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae laudantium officiis nulla corrupti error laborum asperiores sunt nihil placeat cupiditate voluptatum explicabo voluptatem aspernatur, odit amet alias laboriosam unde.Assumenda, incidunt quos! Iusto sapiente voluptatem quaerat. Aspernatur numquam sed culpa doloribus odio, officiis sapiente ratione quaerat illo, laborum ad eaque error et in dolore nisi natus corporis ut consectetur.</p>
+        </div>
+        <section class="container m-5">
+            <h2>Sobre Nós</h2>
             <br>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa officiis fugit 
                     accusamus architecto dignissimos, iusto quibusdam dolorem expedita non dolor 
                     eligendi nobis temporibus voluptas labore eos quos quasi! Et, consectetur.
                 </p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa officiis fugit 
+                    accusamus architecto dignissimos, iusto quibusdam dolorem expedita non dolor 
+                    eligendi nobis temporibus voluptas labore eos quos quasi! Et, consectetur.
+                </p>
+        </section>
+        <section class="container mr-5">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 d-flex flex-column justify-content-center align-items-center">
+                    <img class="foto-perfil" src="img/perfil/aline.jpg" alt="foto perfil">
+                    <h3>Aline</h3>
+                    <p class="texto-perfil">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo impedit, nulla modi non iure necessitatibus aspernatur, tenetur ipsum odit sed nesciunt! Molestiae ab cupiditate voluptate facere iste obcaecati iusto dolores.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  d-flex flex-column justify-content-center align-items-center">
+                    <img class="foto-perfil" src="img/perfil/claudia.jpg" alt="foto perfil">
+                    <h3>Cláudia</h3>
+                    <p class="texto-perfil">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo impedit, nulla modi non iure necessitatibus aspernatur, tenetur ipsum odit sed nesciunt! Molestiae ab cupiditate voluptate facere iste obcaecati iusto dolores.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  d-flex flex-column justify-content-center align-items-center">
+                    <img class="foto-perfil" src="img/perfil/luana.jpg" alt="foto perfil">
+                    <h3>Luana</h3>
+                    <p class="texto-perfil">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo impedit, nulla modi non iure necessitatibus aspernatur, tenetur ipsum odit sed nesciunt! Molestiae ab cupiditate voluptate facere iste obcaecati iusto dolores.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  d-flex flex-column justify-content-center align-items-center">
+                    <img class="foto-perfil" src="img/perfil/chaiana.jpg" alt="foto perfil">
+                    <h3>Chaiana</h3>
+                    <p class="texto-perfil">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo impedit, nulla modi non iure necessitatibus aspernatur, tenetur ipsum odit sed nesciunt! Molestiae ab cupiditate voluptate facere iste obcaecati iusto dolores.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  d-flex flex-column justify-content-center align-items-center">
+                    <img class="foto-perfil" src="img/perfil/ligia.jpg" alt="foto perfil">
+                    <h3>Lígia</h3>
+                    <p class="texto-perfil">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo impedit, nulla modi non iure necessitatibus aspernatur, tenetur ipsum odit sed nesciunt! Molestiae ab cupiditate voluptate facere iste obcaecati iusto dolores.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  d-flex flex-column justify-content-center align-items-center">
+                    <img class="foto-perfil" src="img/perfil/anae.jpg" alt="foto perfil">
+                    <h3>Anaê</h3>
+                    <p class="texto-perfil">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo impedit, nulla modi non iure necessitatibus aspernatur, tenetur ipsum odit sed nesciunt! Molestiae ab cupiditate voluptate facere iste obcaecati iusto dolores.</p>
+                </div>
+            </div>
         </section>
 
     </main>
