cd18ca0c9af7b03068860a4fcdabbbc0a5001fe6
LuanaDH <solimenolua@gmail.com>
2019-10-17 11:25:40 -0300

    HEADER html e css - 1a etapa

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="author" content="Aline Naoe, Anaê Ross, Chaiana Hermes, Claudia Leite, Ligia Pretel Eimantas, Luana Solimeno">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Header - Guardiões Pet</title>
+</head>
+
+<body>
+    <header class="container-fluid">
+        <div class="row">
+        <nav class= "col-12 d-flex align-items-center p-3">
+            <a class= "navbar-brand" href= "#">
+                <img src="img/lodo-dentro.png" width= "50%" height="50%" alt= "">
+            </a>
+      
+        <ul class="col-10 nav d-flex justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Seja um guardião</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sobre nós</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Como funciona</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Parceiros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Doações</a>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li> 
+
+            <button type="button" class="btn btn-success">Cadastre-se</button>
+          </ul>    
+        </nav> 
+        </div> 
+    </header>
+    
+</body>
+</html>
