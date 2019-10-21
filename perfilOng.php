723a60d30508ef98ae594961018fddb9a5429bb2
ClaudiaDH <claudialeite.estudos@gmail.com>
2019-10-21 16:53:52 -0200

    correcao de algumas Ids e add  botao

--- perfilOng.php
+++ perfilOng.php
@@ -12,7 +12,6 @@
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
     <link rel="stylesheet" href="css/styles.css"/>
 
 </head>
@@ -20,61 +19,64 @@
 <body>
 
     <!-- Lugar onde ficara  as informacoes das ONGS e Imgs-->
-    <section id="perfil_ong_info_cx_principal" class="container-fluid">
-        <div class="row" id="perfil_ong_caixa_info">
-            <div class="col-lg-6 col-md-6 " id="perfil_ong_info">
-                <h1 id="perfil_ong_info_titulo">Cia dos Miados e Latidos</h1>
+    <section id="pfong_cx_info_principal" class="container-fluid">
+        <div class="row" id="pfong_caixa_info">
+            <div class="col-lg-6 col-md-6 " id="pfong_info">
+                <h1 id="pfong_info_titulo">Cia dos Miados e Latidos</h1>
                 <p>Nossa ONG nasceu com intenções claras de resgate, castração e doação de animais. Nos empenhamos em abrigar poucos, socorrer vários e castrar MUITOS, pois a castração é a única solução ao problema estrutural da procriação de animais abandonados.</p>
                 <p>Nossa ONG é formada por profissionais liberais, gente comum que trabalha, estuda, enfim, vive!</p> 
-                <div id="perfil_ong_cx_i_txt">
+                <div id="pfong_cx_i_txt" class="d-flex align-items-center">
                     <i class="mdi mdi-email-outline"></i>
                     <p>Quer conversar com a gente?<br> Envie uma mensagem!</p>
                     
                 </div>
                 
             </div>
-            <div class="col-lg-6 col-md-6 " id="perfil_ong_caixa_img">
-                <div id="perfil_ong_img"></div>
+            <div class="col-lg-6 col-md-6 d-flex justify-content-center align-items-center" id="pfong_caixa_img">
+                <div id="pfong_img"></div>
             </div>
         </div>
     </section>
 
     <!-- Lugar onde encontraremos alguns perfis de animais que estao com a ONG-->
-    <section id="perfil_ong_cx_animais" class="container">
+    <section id="pfong_cx_animais" class="container">
         <h1 >Animais com a ONG (98)</h1>
-        <div class="row" id="perfil_ong_animais"> 
-            <div class="col-lg-4 perfil_ong_cards">
-                <div id="perfil_ong_animais_img1"></div>
+        <div class="row" id="pfong_animais"> 
+            <div class="col-lg-4 pfong_cards">
+                <div id="pfong_animais_img1" class="pfong_animais_imgs"></div>
                 <h1>ELZA</h1>
                 <div></div>
                 <p>Tenho uma pelagem de tres cores que é puro charme e pra completar uma carinha inocente que é i-rre-sis-tível!Adoro tomar um solzinho na janela e tirar cochilos na ...</p>
             </div>
-            <div class="col-lg-4 testando perfil_ong_cards">
-                <div id="perfil_ong_animais_img2"></div>
+            <div class="col-lg-4 testando pfong_cards">
+                <div id="pfong_animais_img2" class="pfong_animais_imgs"></div>
                 <h1>ELZA</h1>
                 <p>Tenho uma pelagem de tres cores que é puro charme e pra completar uma carinha inocente que é i-rre-sis-tível!Adoro tomar um solzinho na janela e tirar cochilos na ...</p>
             </div>
-            <div class="col-lg-4 testando perfil_ong_cards">
-                <div id="perfil_ong_animais_img3"></div>
+            <div class="col-lg-4 testando pfong_cards">
+                <div id="pfong_animais_img3" class="pfong_animais_imgs"></div>
                 <h1>ELZA</h1>
                 <p>Tenho uma pelagem de tres cores que é puro charme e pra completar uma carinha inocente que é i-rre-sis-tível!Adoro tomar um solzinho na janela e tirar cochilos na ...</p>
+            </div>
+            <div id="cx-btn" class="d-flex justify-content-end align-items-center">
+                <button>Ver Mais</button>
             </div>
         </div>   
     </section>
 
     <!-- Encontremos aqui, algumas coisas que as Ongs estao precisando-->
-    <section class="container-fluid" id="perfil_ong_precisando_principal">
+    <section class="container-fluid" id="pfong_precisa_cx_principal">
         <h1>Estamos Precisando!</h1>
-        <div class="row"  id="perfil_ong_cx_precisando">
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
-            <div class="perfil_ong_precisando">Antipulgas Bravecto</div>
+        <div class="row"  id="pfong_cx_precisa">
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
+            <div class="pfong_precisando">Antipulgas Bravecto</div>
             
                       
         </div>
