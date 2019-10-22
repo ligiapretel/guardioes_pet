dd61eebfb65109d271d8d150778321e47e000d00
LuanaDH <solimenolua@gmail.com>
2019-10-22 11:23:46 -0300

    arrumando/modificando css header

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet"/>
+    <link href='https://fonts.googleapis.com/css?family=Rockwell' rel='stylesheet' type='text/css'>
     <title>Guardiões Pet</title>
 </head>
 <body>
