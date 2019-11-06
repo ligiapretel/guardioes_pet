5ed0f3d3e76944bc7240b5c28dfcec9696d7c407
Aline <alinenaoe@gmail.com>
2019-11-05 21:36:28 -0300

    ajuste inicial header e nas media queries

--- includes/header.php
+++ includes/header.php
@@ -1,37 +1,50 @@
-    <header class="fixed-top">
-      <div class="row align-items-baseline m-0">
-        <div class="col-3 pl-4">
-          <a class="fonteHeader" href="index.php">
-            <img class="logo" src="img/escudo.png" alt="Guardiões Pet">Guardiões Pet
-          </a>
-        </div>
-        <div class="col-9">
-          <nav id="header-nav">
-            <ul class="nav">
-              <li>
-                <a class="nav-link linkHeader" href='index.php#seja-guardiao-home'>Ser um guardião</a>
-              </li>
-              <li>
-                <a class="nav-link linkHeader" href="sobre.php">Sobre nós</a>
-              </li>
-              <li>
-                <a class="nav-link linkHeader" href='index.php#como-funciona-home'>Como funciona</a>
-              </li>
-              <li>
-                <a class="nav-link linkHeader" href="parceiros.php">Parceiros</a>
-              </li>
-              <li>
-                <a class="nav-link linkHeader" href="anuncios.php">Doações</a>
-              </li>
-              <li>
-                <a class="nav-link linkHeader" href="index.php#footer">Contato</a>
-              </li>
-              <li>
-                <a class="nav-link linkHeader" href="login.php">Login</a>
-              </li>
-              <a href="cadastre_se.php"><button type="button" class="btnHeader">Cadastre-se</button></a>
-            </ul>
-        </div>
+<header class="fixed-top">
+
+    <div class="container col-11">
+
+    <nav class="navbar navbar-expand flex-column flex-md-row bd-navbar">
+        <!-- checar depois a classe fonteHeader -->
+        <a class="fonteHeader navbar-brand" href="index.php">
+            <img src="img/escudo.png" alt="Guardiões Pet" width="50" class="d-inline-block align-center">Guardiões Pet
+        </a>
+
+            <!-- checar depois - para criar menu-hamburger -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- checar o que faz essa id -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav flex-row  ml-md-auto d-none d-md-flex"> 
+
+
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href='index.php#seja-guardiao-home'>Ser um guardião</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link linkHeader" href="sobre.php">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link linkHeader" href='index.php#como-funciona-home'>Como funciona</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link linkHeader" href="parceiros.php">Parceiros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link linkHeader" href="anuncios.php">Doações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link linkHeader" href="index.php#footer">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link linkHeader" href="login.php">Login</a>
+                        <li class="nav-item">
+                    <a href="cadastre_se.php"><button type="button" class="btnHeader">Cadastre-se</button></a>
+                </ul>
+
+            </div>
+
         </nav>
-      </div>
-    </header>
+        </div>
+</header>
