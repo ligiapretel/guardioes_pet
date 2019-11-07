ec06e4395b53ca102a1aca8bb8861a4bd92d2621
Aline <alinenaoe@gmail.com>
2019-11-06 22:28:50 -0300

    header responsivo

--- includes/header.php
+++ includes/header.php
@@ -1,46 +1,43 @@
 <header class="fixed-top">
 
-    <div class="container col-11">
+    <div class="container col-lg-12 col-md-11 col-sm-12 pt-2 pb-2">
 
-    <nav class="navbar navbar-expand flex-column flex-md-row bd-navbar">
-        <!-- checar depois a classe fonteHeader -->
-        <a class="fonteHeader navbar-brand" href="index.php">
-            <img src="img/escudo.png" alt="Guardiões Pet" width="50" class="d-inline-block align-center">Guardiões Pet
+    <nav class="navbar navbar-expand-lg navbar-light header-menu">
+
+    <a class="fonteHeader" href="index.php">
+            <img src="img/escudo.png" alt="Guardiões Pet" width="35" class="d-inline-block align-center"> Guardiões Pet
         </a>
 
-            <!-- checar depois - para criar menu-hamburger -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
 
-            <!-- checar o que faz essa id -->
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 
-                <ul class="navbar-nav flex-row  ml-md-auto d-none d-md-flex"> 
-
+            <ul class="navbar-nav ml-md-auto mt-2 mt-lg-0">
 
                     <li class="nav-item">
-                        <a class="nav-link text-white" href='index.php#seja-guardiao-home'>Ser um guardião</a>
+                        <a class="nav-link header-link" href='index.php#seja-guardiao-home'>Ser um guardião</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link linkHeader" href="sobre.php">Sobre nós</a>
+                        <a class="nav-link header-link" href="sobre.php">Sobre nós</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link linkHeader" href='index.php#como-funciona-home'>Como funciona</a>
+                        <a class="nav-link header-link" href='index.php#como-funciona-home'>Como funciona</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link linkHeader" href="parceiros.php">Parceiros</a>
+                        <a class="nav-link header-link" href="parceiros.php">Parceiros</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link linkHeader" href="anuncios.php">Doações</a>
+                        <a class="nav-link header-link" href="anuncios.php">Doações</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link linkHeader" href="index.php#footer">Contato</a>
+                        <a class="nav-link header-link" href="index.php#footer">Contato</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link linkHeader" href="login.php">Login</a>
+                        <a class="nav-link header-link" href="login.php">Login</a>
                         <li class="nav-item">
-                    <a href="cadastre_se.php"><button type="button" class="btnHeader">Cadastre-se</button></a>
+                    <a href="cadastre_se.php"><button type="button" class="btn-roxo">Cadastre-se</button></a>
                 </ul>
 
             </div>
