1390522e1a6687f6365242431fa4e4fd6cf5b94a
Anaê  Soares <anae08@gmail.com>
2019-11-09 18:00:12 -0300

    realizado menu lateral da ong para responsivo com menu que recolhe, incluido no max-width 768, feito o css do menu recolhido também

--- includes/menu-ong.php
+++ includes/menu-ong.php
@@ -6,29 +6,42 @@
     <meta name="description" content="Adote, abrigue, ajude. Seja um guardião pet. Encontre o cão ou gato que combina com seu perfil e ajude ONGs a transformarem a vida desses bichinhos.">
     <meta name="author" content="Aline Naoe, Anaê Ross, Chaiana Hermes, Claudia Leite, Ligia Pretel Eimantas, Luana Solimeno">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">		
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="img/escudo.png" sizes="32x32" rel="shortcut icon"/>
     <link href="css/styles.css" rel="stylesheet"/>
-    <link href='https://fonts.googleapis.com/css?family=Rockwell' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Rockwell' rel='stylesheet' type='text/css'>    
     <title>Guardiões Pet</title>
 </head>
-<body>   
-    
-    <aside id="menu-ong" class="col-lg-3 col-md-3 col-sm-12 col-xs-12 p-5">
-        <div class="card" style="width: 20rem;">
-            <img id="perfil-ong" src="./img/foto_perfil_ong.jpg" alt="logo da ONG" />
-            <div class="card-body" style="width: 20rem;">
-                <nav class="nav flex-column bg-light">
-                    <a class="nav-link menu-ong active" href="#">Meu Perfil</a>
-                    <a class="nav-link menu-ong" href="#">Animais Cadastrados</a>
-                    <a class="nav-link menu-ong" href="#">Cadastrar Novos Animais</a>
-                    <a class="nav-link menu-ong" href="#">Anúncios Cadastrados</a>
-                    <a class="nav-link menu-ong" href="#">Cadastrar Novos Anúncios</a>
-                    <a class="nav-link menu-ong" href="#">Mensagens</a>
-                    <a class="nav-link menu-ong-sair" href="#">Sair</a>
-                </nav>
-            </div>
-        </div>
-    </aside>
-</body>
+<body>       
+    <aside id="menu-ong" class="col-lg-3 col-md-3 col-sm-10 col-xs-8 p-5">
+        <a href="#menu-principal" data-toggle="collapse" aria-expanded="false" class="navbar-menu-principal collapsed col-12">
+            <i class="fa fa-bars"></i>
+        </a>	 
+        <div class="card navbar-menu-itens collapse" style="width: 20rem;" id="menu-principal">
+                <img id="perfil-ong" class="ml-4" src="img/foto_perfil_ong.jpg" alt="logo da ONG" />
+                <div class="card-body" style="width: 20rem;">
+                        <nav class="nav flex-column bg-light">
+                            <a class="nav-link menu-ong active" href="#">Meu Perfil</a>
+                            <a class="nav-link menu-ong" href="#">Animais Cadastrados</a>
+                            <a class="nav-link menu-ong" href="#">Cadastrar Novos Animais</a>
+                            <a class="nav-link menu-ong" href="#">Anúncios Cadastrados</a>
+                            <a class="nav-link menu-ong" href="#">Cadastrar Novos Anúncios</a>
+                            <a class="nav-link menu-ong" href="#">Mensagens</a>
+                            <a class="nav-link menu-ong-sair" href="#">Sair</a>
+                        </nav>
+                        
+                    </div>
+                </div>
+            </aside>
+            
+            <!-- ini +++++++++++++++++++++++++++++++++++++++++++ -->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            <!-- end +++++++++++++++++++++++++++++++++++++++++++ -->
+            
+            
+        </body>
 </html>
