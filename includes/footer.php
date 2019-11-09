9b9902e1264905b598ff327c9f659fafd3668843
Aline <alinenaoe@gmail.com>
2019-11-09 11:52:09 -0300

    footer responsivo

--- includes/footer.php
+++ includes/footer.php
@@ -1,52 +1,53 @@
-    <footer id="footer" class="coresFooter">
-        <section class="container">
-            <div class="row pt-4">
-                <div class="col-7 d-flex">
-                    <ul class="col-4 align-content-around list-unstyled">
-                        <li class="espaco"><a class="linkFooter" href="index.php">Home</a></li>
-                        <li class="espaco"><a class="linkFooter" href="index.php#seja-guardiao-home">Ser um guardião</a>
-                        </li>
-                        <li class="espaco"><a class="linkFooter" href="sobre.php">Sobre nós</a></li>
-                        <li class="espaco"><a class="linkFooter" href="index.php#como-funciona-home">Como funciona?</a>
-                        </li>
-                        <li class="espaco"><a class="linkFooter" href="parceiros.php">Parceiros</a></li>
-                        <li class="espaco"><a class="linkFooter" href="anuncios.php">Doações</a></li>
-                        <a href="https://www.facebook.com/"><img class="img-fluid mt-5 p-2" src="img/facebook1.png"
-                                width="25%" alt="facebook"></a>
-                        <a href="https://www.instagram.com/"><img class="img-fluid mt-5 p-2" src="img/instagram1.png"
-                                width="25%" alt="instagram"></a>
-                        <a href="https://www.twitter.com/"><img class="img-fluid mt-5 p-2" src="img/twitter1.png"
-                                width="25%" alt="twitter"></a>
+<footer>
+    <section class="container">
+        <div class="row pt-4">
+            <div class="col-lg-7 col-md-7 col-sm-12">
+                <div class="row">
 
+                    <ul class="col-lg-4 col-md-5 col-sm-6 align-content-around list-unstyled">
+                        <li><a class="menu-link" href="index.php">Home</a></li>
+                        <li><a class="menu-link" href="index.php#seja-guardiao-home">Ser um guardião</a></li>
+                        <li><a class="menu-link" href="sobre.php">Sobre nós</a></li>
+                        <li><a class="menu-link" href="index.php#como-funciona-home">Como funciona?</a></li>
+                        <li><a class="menu-link" href="parceiros.php">Parceiros</a></li>
+                        <li><a class="menu-link" href="anuncios.php">Doações</a></li>
                     </ul>
 
-                    <ul class="col-8 list-unstyled">
-                        <li class="espaco"><a class="linkFooter" href="#">Perguntas frequentes</a></li>
-                        <li class="espaco"><a class="linkFooter" href="#">Política de privacidade</a></li>
-                        <li class="espaco"><a class="linkFooter" href="#">Termos de uso</a></li>
+                    <ul class="col-lg-8 col-md-7 col-sm-6 list-unstyled">
+                        <li><a class="menu-link" href="#">Perguntas frequentes</a></li>
+                        <li><a class="menu-link" href="#">Política de privacidade</a></li>
+                        <li><a class="menu-link" href="#">Termos de uso</a></li>
                     </ul>
-                </div>
 
-                <div class="col-5 pr-5">
-                    <h5>Quer conversar com a gente?</h5><br>
-                    <form action="" method="POST">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="nome" placeholder="Nome">
-                        </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control" name="email" placeholder="E-mail">
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3" placeholder="Mensagem"></textarea>
-                        </div>
-                        <button class="btnFooter" type="submit">Enviar</button>
-                    </form>
-                </div>
+                </div>    
 
-                <div class="col-3 mb-5">
-                </div>
+                <div class="row">
+                    <div class="col-12 pt-5 pb-5">
+                        <a href="https://www.facebook.com/"><img src="img/facebook1.png" alt="facebook"></a>
+                        <a href="https://www.instagram.com/"><img src="img/instagram1.png" alt="instagram"></a>
+                        <a href="https://www.twitter.com/"><img src="img/twitter1.png" alt="twitter"></a>                    
+                    </div>
+                </div> 
 
             </div>
+                                            
+            <div class="col-lg-5 col-md-5 col-sm-12 pr-5">
+                <h5>Quer conversar com a gente?</h5><br>
+                <form action="chat.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="nome" placeholder="Nome">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" name="email" placeholder="E-mail">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" rows="3" placeholder="Mensagem"></textarea>
+                    </div>
+                    <button class="btn-roxo" type="submit">ENVIAR</button>
+                </form>
+            </div>
 
-        </section>
-    </footer>
+        </div>
+
+    </section>
+</footer>
