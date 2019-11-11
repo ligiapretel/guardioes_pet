c53bbf1823f85152bc48d5fc3dff9b91f6a323c8
Chaiana Hermes <chaianah@gmail.com>
2019-11-11 14:22:15 -0300

    deixando as páginas index e login responsivas

--- login.php
+++ login.php
@@ -15,29 +15,29 @@
 <body>
     <!-- Incluindo header -->
     <?php include_once("includes/header.php")?>
-    <main id="main-login">
-        <div id="container-form-login" class="col-4">
+    <main id="main-login" class="col-lg-12 col-md-12 col-sm-12 col-xs-6">
+        <div id="container-form-login" class="col-lg-5 col-md-6 col-sm-6 col-xs-6">
             <form action="#" method="POST">
                 <div class="row justify-content-center">
-                    <div class="col-10 text-center">
+                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-6 text-center">
                         <h1>Acesse sua conta</h1>
                     </div>
-                    <div class="col-10 p-2">
+                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-6 p-2">
                         <input class="form-control" type="text" name="login" placeholder="E-mail">
                     </div>
-                    <div class="col-10 p-2">
+                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-6 p-2">
                         <input class="form-control" type="password" name="senha" placeholder="Senha">
                     </div>
-                    <div class="col-10 p-2">
+                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-6 p-2">
                         <button class="btn-busca-expandida" type="submit" name="procurar">Entrar</button>
                     </div>
                 </div>
                 <div class="row justify-content-center align-items-baseline">
-                    <div class="col-5 p-2">
+                    <div class="sobre-senha col-lg-5 col-md-5 col-sm-5 col-xs-5 p-2">
                         <input type="checkbox" name="lembrar-dados" value="aceita-lembrar-dados">
                         <label class="label-lembrar-dados">Lembrar dados</label>
                     </div>
-                    <div class="col-5 p-2">
+                    <div class="sobre-senha col-lg-5 col-md-5 col-sm-5 col-xs-5 p-2">
                         <a class="esqueci-senha" href="#">
                             <p>Esqueci a senha</p>
                         </a>
@@ -45,14 +45,13 @@
                 </div>
             </form>
             <div class="row justify-content-center">
-                <div class="col-10 text-center">
+                <div class="col-lg-10 col-md-10 col-sm-10 col-xs-6 text-center">
                     <h2>ou cadastre-se</h2>
                 </div>
-                <div class="col-5 p-2 text-center">
-                    <a href="cadastroOng.php"><button class="btn-login-enviar" type="submit" name="sou-ong">Sou
-                            ONG</button></a>
+                <div class="col-lg-5 col-md-10 col-sm-10 col-xs-5 p-2 m-1 text-center">
+                    <a href="cadastroOng.php"><button class="btn-login-enviar" type="submit" name="sou-ong">Sou ONG</button></a>
                 </div>
-                <div class="col-5 p-2 text-center">
+                <div class="col-lg-5 col-md-10 col-sm-10 col-xs-5 p-2 m-1 text-center">
                     <a href="cadastroGuardiao.php"><button class="btn-login-enviar" type="submit"
                             name="sou-guardiao">Sou Guardião</button></a>
                 </div>
