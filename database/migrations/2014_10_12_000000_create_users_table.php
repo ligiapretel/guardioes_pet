a7103149a9ad8467dddbcc594a224dd09ec6db56
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2019-12-10 11:02:46 -0300

    Ajustando tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedBigInteger('user_group_id');
+            $table->foreign('user_group_id')->references('id')->on('users_group');
+            $table->unsignedBigInteger('status_id');
+            $table->foreign('status_id')->references('id')->on('status');
             $table->timestamps();
         });
     }
