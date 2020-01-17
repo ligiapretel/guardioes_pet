0c60e0f9b6fb2ebe694013bc5a1e6d13d41f37d5
Aline <alinenaoe@gmail.com>
2020-01-16 22:02:13 -0300

    inserindo valores padrão nas migrations de status e user_group

--- database/migrations/2013_12_10_134652_create_status_table.php
+++ database/migrations/2013_12_10_134652_create_status_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Status;
 
 class CreateStatusTable extends Migration
 {
@@ -18,6 +19,11 @@ class CreateStatusTable extends Migration
             $table->string('status',45);
             $table->timestamps();
         });
+
+        Status::insert([
+            ['status' => 'ativo'],
+            ['status' => 'inativo']
+        ]);
     }
 
     /**
