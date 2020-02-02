3803583bcab77d1e20e8a401b9307bc8b59bebb6
Anaê  Soares <anae08@gmail.com>
2020-02-02 18:05:15 -0300

    alterado frase de valição de campo requerido

--- public/js/parsley.js
+++ public/js/parsley.js
@@ -2392,7 +2392,7 @@
         alphanum: "This value should be alphanumeric."
       },
       notblank: "This value should not be blank.",
-      required: "This value is required.",
+      required: "Esse valor é requerido",
       pattern: "This value seems to be invalid.",
       min: "This value should be greater than or equal to %s.",
       max: "This value should be lower than or equal to %s.",
